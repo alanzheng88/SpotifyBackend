cf3297807f92d29e51fa3db9e3ba7481a27e05c2
Alan Zheng <alanz@sfu.ca>
2018-06-06 13:18:44 -0700

    add example config

---
+++ app-env-example.sh
@@ -0,0 +1,7 @@
+export NODE_ENV="dev"
+export HOSTNAME="myapp.local"
+export PORT=3000
+export SPOTIFY_REDIRECT_URI="http://myapp.local:3000/api/v1/login/callback"
+export SPOTIFY_CLIENT_ID=""
+export SPOTIFY_CLIENT_SECRET=""
+export GOOGLE_API_KEY=""
